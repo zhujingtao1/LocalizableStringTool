aedf43f06b23d079e3a0895bc7e34f03eb68d51f
jingtao.zhu <jingtao.zhu@aiswei-tech.com>
2026-02-28 11:23:21 +0800

    测试

--- LocalizableTool/AppDelegate.swift
+++ LocalizableTool/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    //测试
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
